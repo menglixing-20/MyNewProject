c8dce3b28a836c59a02033979959f95532efe17b
mengl <mengl@Menglixing>
2024-09-21 17:46:55 +0800

    First

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import java.math.BigDecimal;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BigDecimal big = new BigDecimal("55555555556666666666666");
+		
+		BigDecimal verybig = big.pow(2);
+		System.out.println(verybig);
+	}
+
+}
